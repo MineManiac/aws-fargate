674bc8fb1861d6e594cead0ac8a145edc3220024
MineManiac <matheusbarrosr@gmail.com>
2023-05-26 18:22:30 -0300

    permissions for ecs

---
+++ terraform/ecs_role.tf
@@ -0,0 +1,37 @@
+resource "aws_iam_role" "ecs_role" {
+  name = "ecs-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "task_role" {
+  name = "task-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
